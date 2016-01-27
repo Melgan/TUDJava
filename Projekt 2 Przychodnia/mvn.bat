c4a02442e9b727258b279d6eb3da7b8c1e6d4c3a
Melgan <melgan7@gmail.com>
2016-01-27 20:17:17 +0100

    fragment

---
+++ Projekt 2 Przychodnia/mvn.bat
@@ -0,0 +1,3 @@
+cd C:\Users\mgancza\Documents\GitHub\TUDJava\Przychodnia
+mvn test > C:\Users\mgancza\Desktop\testy.txt
+pause
---
+++ Projekt 2 Przychodnia/server.bat
@@ -0,0 +1,6 @@
+
+cd C:\Users\mgancza\Documents\GitHub\TUDJava\Przychodnia
+
+java -cp hsqldb.jar org.hsqldb.server.Server
+
+pause
