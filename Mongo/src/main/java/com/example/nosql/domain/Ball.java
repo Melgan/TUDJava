230978ea3ac71ffb5fd7617112b809b28ec5c238
Melgan <melgan7@gmail.com>
2016-02-03 19:52:43 +0100

    ball

---
+++ Mongo/src/main/java/com/example/nosql/domain/Ball.java
@@ -0,0 +1,51 @@
+package com.example.nosql.domain;
+
+import org.bson.types.ObjectId;
+
+public class Ball {
+	
+	
+	private ObjectId id;
+	private String name;
+	private String color;
+	private String type;
+	private int size;
+
+	
+	public ObjectId getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
